6646655f54510687f517816f5321420f94563a5d
Yani Rodriguez <yani010712@gmail.com>
2020-05-16 17:00:26 -0400

    creating test layer and framework layer using page object model pattern

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,34 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
+
+import java.util.List;
+
+public class BaseTests {
+    /* Create a WEBDriver object */
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        /*Instantiate the driver as a ChromeDriver */
+        driver = new ChromeDriver();
+        /*Launch The Browser*/
+        driver.get("https://the-internet.herokuapp.com/");
+
+        homePage = new HomePage(driver);
+    }
+
+    @AfterClass
+    public void  tearDown(){
+        driver.quit();
+    }
+
+}
